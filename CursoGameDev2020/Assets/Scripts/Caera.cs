0ac870f616fc5297050f6bd1b07209c282b8354a
israel05 <i.delamo@gmail.com>
2021-04-25 23:13:19 +0200

    Añadidas una bola y una pendiente

--- CursoGameDev2020/Assets/Scripts/Caera.cs
+++ CursoGameDev2020/Assets/Scripts/Caera.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class Caera : MonoBehaviour
 {
+
+    public float tiempoAntesDeCaida = 3f;
     // Start is called before the first frame update
     void Start()
     {
+        //comienza oculto y sin gravedad
         GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<Rigidbody>().useGravity = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(Time.time);
-        if (Time.time > 3)
+       // Debug.Log(Time.time);
+        if (Time.time > tiempoAntesDeCaida)
         {
             GetComponent<Rigidbody>().useGravity = true;
             GetComponent<MeshRenderer>().enabled = true;
--- CursoGameDev2020/Assets/Scripts/ObjectHit.cs
+++ CursoGameDev2020/Assets/Scripts/ObjectHit.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class ObjectHit : MonoBehaviour
 {
     public int numeroDeColisiones = 0;
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision other)
     {
-        
-        //Del componente que ha recibido la colisión, coge su meshrenderer. Del meshRenderer coge el material. Del Material coge su color
-        GetComponent<MeshRenderer>().material.color = Color.red;
-        numeroDeColisiones++;
-        Debug.Log("Colisión número:" + numeroDeColisiones);
+        //Detecta que es Player el que toca
+        if(other.gameObject.tag  == "Player")
+        {
+            Debug.Log("Player ha tocado");
+            //Del componente que ha recibido la colisión, coge su meshrenderer. Del meshRenderer coge el material. Del Material coge su color
+            GetComponent<MeshRenderer>().material.color = Color.red;
+            numeroDeColisiones++;
+            Debug.Log("Colisión número:" + numeroDeColisiones);
+        }
+
+       
     }
 }
